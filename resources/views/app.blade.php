ba551ed7940e90c2d8f6cbb3b0d0d295ee9411a1
Polycarp29 <fb.admin87@protonmail.com>
2026-02-05 16:43:42 +0300

    More changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <x-s-e-o-aspects :title="$title ?? 'Metapilot – The All-in-One SEO, Schema, and GEO Optimization Platform'">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+<head>
+    <x-s-e-o-aspects :title="$title ?? 'Metapilot – The All-in-One SEO, Schema, and GEO Optimization Platform'">
+    </x-s-e-o-aspects>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
