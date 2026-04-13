8b2d32604699cb864bc8974c4848ef276ffbde80
Polycarp29 <fb.admin87@protonmail.com>
2026-04-13 12:05:01 +0300

    Pending feature for pixel data

---
+++ resources/js/Pages/Analytics/Partials/Signals.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppLayout from '../../../Layouts/AppLayout.vue';
+
+</script>
+
+<template>
+    <AppLayout>
+        <!--We will include signal data here-->
+    </AppLayout>
+</template>
